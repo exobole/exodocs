1fd2bae4ee888a0bf08249014f2a1e2665f74fad
LouisLec <louis.l@obole-digitale.fr>
2020-10-20 22:56:22 +0200

    feat: configure the project for i18n + change primary color

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,25 @@
-import theme from '@nuxt/content-theme-docs'
+import theme from "@nuxt/content-theme-docs";
 
 export default theme({
   docs: {
-    primaryColor: '#E24F55'
-  }
-})
+    primaryColor: "#0FA3B1",
+  },
+  loading: { color: "#00CD81" },
+  i18n: {
+    locales: () => [
+      {
+        code: "fr",
+        iso: "fr-FR",
+        file: "fr-FR.js",
+        name: "Français",
+      },
+      {
+        code: "en",
+        iso: "en-US",
+        file: "en-US.js",
+        name: "English",
+      },
+    ],
+    defaultLocale: "fr",
+  },
+});
